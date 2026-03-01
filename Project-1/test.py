ea480b933d770fcd5b6ba722f2920d66d40176fb
kaustav <kaustavbasu97@gmail.com>
2026-03-02 00:35:16 +0530

    note added for course 2

--- Project-1/test.py
+++
@@ -1,8 +0,0 @@
-import time
-
-FREQUENCY_PER_MINUTE = 60 # Number of iterations per minute
-
-for i in range(5):
-    print(f"Iteration {i + 1}")
-    time.sleep(60 / FREQUENCY_PER_MINUTE) # Sleep to maintain the frequency of 45 iterations per minute where 60 seconds in a minute and FREQUENCY_PER_MINUTE is 45
-
